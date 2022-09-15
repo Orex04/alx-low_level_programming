aa3991d93a3d193d3c9dca73afd01768576ad607
Orelesi Elijah <elijahorelesi@gmail.com>
2022-09-14 17:56:48 -0700

    low level programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
- * print_alphabet_x0 - prints the lowercase alphabet 10 times on new lines
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times on new lines
  *
  * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	char c = 'a';
+	char c ='a';
 	int i, j;
 
-
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-        	for (j = 0; j < 26; j++)
+		for (j = 0; j < 26;, j++)
 		{
 			_putchar(c + j);
 		}
-        	_putchar(10);
+		_putchar(10);
 	}
 }
