2436c80b6a56bf9b81330c6ec5048c8c417b1d51
Orelesi Elijah <elijahorelesi@gmail.com>
2022-09-14 19:59:29 -0700

    changes

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints times table for 9
+ *
+ */
+
+void times_table(void)
+{
+	int x =; /* factor */
+	int y; /* count */
+	int z; /* computed value*/
+
+	while (x < 10)
+	{
+		z = x * y;
+
+		if ( z > y)
+		{
+			_putchar(z / 10 + '0');
+			_putchar(z % 10 +'0');
+		}
+		else if (y != 0)
+		{
+			_putchar('');
+			_putchar(z + '0');
+		}
+
+		if (y != 9)
+		{
+			_putchar(',');
+			_putchar('');
+		}
+		y++;
+	}
+	_putchar('\n');
+	x++;
+}
