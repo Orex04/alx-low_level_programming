b46063ce01041e86e954c4e82954e435e23a342a
Orelesi Elijah <elijahorelesi@gmail.com>
2022-09-18 19:35:54 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 48, a = 48;
+
+	while (d < 58)
+	{
+		a = 48;
+		whiel (a < 58)
+		{
+			if (d != a && d < a)
+			{
+				putchar(d);
+				putchar(a);
+
+				if (!(d == 5 && a == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a++;
+		}
+		d++;
+	}
+	putchar(''\n');
+
+	return (0);
+}
