dfab7f4943e446bf266d83b540f5af5a90d6f7a0
Orelesi Elijah <elijahorelesi@gmail.com>
2022-09-19 22:12:36 -0700

    low level

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints first n elements of array
+ * @a: int arr to display form
+ * @n: number of elements to display
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+		i++;
+	}
+	printf("\n");
+}
