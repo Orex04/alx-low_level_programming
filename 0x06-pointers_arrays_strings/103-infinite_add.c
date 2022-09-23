e4a55ff9cbf63d4455b5befe0817ed024d89b095
Orelesi Elijah <elijahorelesi@gmail.com>
2022-09-23 07:22:03 -0700

    function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@
  *
  * Return: address of r with 0
  */
-char *infinite_add(char *n1, char *n2, charb*r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
@@ -35,9 +35,9 @@ char *infinite_add(char *n1, char *n2, charb*r, int size_r)
 		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
-	if (i >= 0 ||k j >= 0 || m)
-		return 900;
-	for (k -= 1, 1 =0; 1 < k; k--, l++)
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, 1 = 0; 1 < k; k--, l++)
 	{
 		m = r[k];
 		r[k] = r[l];
