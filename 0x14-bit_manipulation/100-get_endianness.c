02137782e756d417066d38126c7c0cd8a7cf8211
Orelesi Elijah <elijahorelesi@gmail.com>
2022-10-28 19:50:58 -0700

    changes

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,7 +8,7 @@ int get_endianness(void)
 {
 	int num;
 
-	num = i;
+	num = 1;
 	if (*(char *)&num == 1)
 		return (1);
 	else
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * clear_bit - set the value of abit to 0 at a given index
+ * cle
+ * ar_bit - set the value of a bit to 0 at a given index
  * @n: pointer to decimal number to change
  * @index: index position to change
  * Return: 1 if i worked, -1 if error
@@ -14,7 +15,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 	hold = index;
-	for (i = 1; hold > 0; i *= 2; hold--)
+	for (i = 1; hold > 0; i *= 2, hold--)
 		;
 
 	if ((*n >> index) & 1)
