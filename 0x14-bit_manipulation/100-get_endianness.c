1f833b2573bd371bdff1da54e72cf532839a03e5
Orelesi Elijah <elijahorelesi@gmail.com>
2022-10-28 18:35:49 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * get_endianness - check endianess
+ * Return: 0 if big endian, or 1 if little endian
+ */
+int get_endianness(void)
+{
+	int num;
+
+
+	num = i;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
